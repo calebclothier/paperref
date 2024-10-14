6cf9609e16aa2299115f4914a2ff298c20e1838d
Caleb Clothier <calebclothier@gmail.com>
2024-10-13 23:09:43 -0400

    add: docstrings to backend functions

--- paperref/backend/app/schemas/auth.py
+++ paperref/backend/app/schemas/auth.py
@@ -3,11 +3,13 @@ from pydantic import BaseModel
 
 
 class AuthRequest(BaseModel):
+    """Model for authentication requests."""
     email: str
     password: str
 
 
 class AuthResponse(BaseModel):
+    """Model for authentication responses."""
     token: Optional[str] = None
     message: Optional[str] = None
     
