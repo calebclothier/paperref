d40d3f0b4ad626111606ab9aae69f07415f9074d
Caleb Clothier <calebclothier@gmail.com>
2024-10-17 14:33:19 -0400

    remove: unused comments

--- frontend/src/api/library.py
+++ frontend/src/api/library.py
@@ -50,22 +50,3 @@ def save_library_for_user():
         st.error(f'{message}')
     return None
     
-    
-
-# # mock data
-# test_data = [
-#     [
-#         '10.1103/PhysRevX.12.021028',
-#         'Universal gate operations on nuclear spin qubits in an optical tweezer array of 171Yb atoms', 
-#     ],
-#     [
-#         '10.1038/s41467-022-32094-6', 
-#         'Erasure conversion for fault-tolerant quantum computing in alkaline earth Rydberg atom arrays', 
-#     ],
-#     [
-#         '10.1038/s41586-023-06438-1', 
-#         'High-fidelity gates with mid-circuit erasure conversion in a metastable neutral atom qubit', 
-#     ]
-# ]
-# df = pd.DataFrame(test_data, columns=['DOI', 'Title'])
-# return df
