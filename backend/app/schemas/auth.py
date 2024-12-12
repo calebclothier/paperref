b94c2720600791fdadf61c0569c65723c1cb5391
Caleb Clothier <calebclothier@gmail.com>
2024-12-12 18:04:28 -0500

    Add: docstrings to backend functions (#9)
    * add: docstrings to backend functions
    
    * fix: took out some typo
    
    ---------
    
    Co-authored-by: Marti Vives <mvives1122@gmail.com>

--- backend/app/schemas/auth.py
+++ backend/app/schemas/auth.py
@@ -2,11 +2,13 @@ from pydantic import BaseModel
 
 
 class AuthRequest(BaseModel):
+    """Model for authentication requests."""
     email: str
     password: str
 
 
 class AuthResponse(BaseModel):
+    """Model for authentication responses."""
     id_token: str
     refresh_token: str
     expires_in: str
