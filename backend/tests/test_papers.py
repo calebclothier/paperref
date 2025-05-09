1cb02aed7509ffbf93797be1d8bc522d8f551cca
Caleb Clothier <calebclothier@gmail.com>
2025-05-09 00:42:56 -0400

    fix linting errors

--- backend/tests/test_papers.py
+++ backend/tests/test_papers.py
@@ -10,22 +10,31 @@ def test_add_paper(user_id_token):
         "Authorization": f"Bearer {user_id_token}",
     }
     payload = {
-        'id': '43f52802fc640cb74e9c742fb6f1d272cd17cec6', 
-         'title': 'High-fidelity gates and mid-circuit erasure conversion in an atomic qubit', 
-         'doi': '10.1038/s41586-023-06438-1', 
-         'arxiv': None, 
-         'authors': [
-             'Shuo Ma', 'Genyue Liu', 'Pai Peng', 'Bichen Zhang',
-             'Sven Jandura', 'J. Claes', 'Alex P. Burgers', 'G. Pupillo', 
-             'S. Puri', 'Jeff D. Thompson'], 
-         'abstract': None, 
-         'year': 2023, 
-         'publication_date': '2023-10-01', 
-         'reference_count': 58, 
-         'citation_count': 133, 
-         'journal': 'Nature', 
-         'open_access_url': '', 
-         'tldr': None}
+        "id": "43f52802fc640cb74e9c742fb6f1d272cd17cec6",
+        "title": "High-fidelity gates and mid-circuit erasure conversion in an atomic qubit",
+        "doi": "10.1038/s41586-023-06438-1",
+        "arxiv": None,
+        "authors": [
+            "Shuo Ma",
+            "Genyue Liu",
+            "Pai Peng",
+            "Bichen Zhang",
+            "Sven Jandura",
+            "J. Claes",
+            "Alex P. Burgers",
+            "G. Pupillo",
+            "S. Puri",
+            "Jeff D. Thompson",
+        ],
+        "abstract": None,
+        "year": 2023,
+        "publication_date": "2023-10-01",
+        "reference_count": 58,
+        "citation_count": 133,
+        "journal": "Nature",
+        "open_access_url": "",
+        "tldr": None,
+    }
     response = client.post("/library/papers/add", headers=headers, json=payload)
     assert response.status_code == 200
 
@@ -38,22 +47,32 @@ def test_load_papers(user_id_token):
     response = client.get("/library/papers", headers=headers)
     assert response.status_code == 200
     assert response.json() == [
-        {'id': '43f52802fc640cb74e9c742fb6f1d272cd17cec6', 
-         'title': 'High-fidelity gates and mid-circuit erasure conversion in an atomic qubit', 
-         'doi': '10.1038/s41586-023-06438-1', 
-         'arxiv': None, 
-         'authors': [
-             'Shuo Ma', 'Genyue Liu', 'Pai Peng', 'Bichen Zhang',
-             'Sven Jandura', 'J. Claes', 'Alex P. Burgers', 'G. Pupillo', 
-             'S. Puri', 'Jeff D. Thompson'], 
-         'abstract': None, 
-         'year': 2023, 
-         'publication_date': '2023-10-01', 
-         'reference_count': 58, 
-         'citation_count': 133, 
-         'journal': 'Nature', 
-         'open_access_url': '', 
-         'tldr': None}
+        {
+            "id": "43f52802fc640cb74e9c742fb6f1d272cd17cec6",
+            "title": "High-fidelity gates and mid-circuit erasure conversion in an atomic qubit",
+            "doi": "10.1038/s41586-023-06438-1",
+            "arxiv": None,
+            "authors": [
+                "Shuo Ma",
+                "Genyue Liu",
+                "Pai Peng",
+                "Bichen Zhang",
+                "Sven Jandura",
+                "J. Claes",
+                "Alex P. Burgers",
+                "G. Pupillo",
+                "S. Puri",
+                "Jeff D. Thompson",
+            ],
+            "abstract": None,
+            "year": 2023,
+            "publication_date": "2023-10-01",
+            "reference_count": 58,
+            "citation_count": 133,
+            "journal": "Nature",
+            "open_access_url": "",
+            "tldr": None,
+        }
     ]
 
 
